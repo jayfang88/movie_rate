29884a8626df52788d64c1fea07cd6676d122a92
jayfang88 <jayfang88@gmail.com>
2020-07-09 18:15:48 -0700

    add user auth

--- Gemfile
+++ Gemfile
@@ -34,10 +34,6 @@ gem 'jbuilder', '~> 2.5'
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.1.0', require: false
 
-gem 'better_errors'
-gem 'binding_of_caller'
-gem 'pry-rails'
-gem 'annotate'
 gem 'bcrypt'
 gem 'jquery-rails'
 
@@ -53,6 +49,10 @@ group :development do
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
+  gem 'better_errors'
+  gem 'binding_of_caller'
+  gem 'pry-rails'
+  gem 'annotate'
 end
 
 group :test do
---
+++ app/models/user.rb
@@ -0,0 +1,34 @@
+class User < ApplicationRecord
+    validates :username, uniqueness: true
+    validates :username, :fname, :lname, :session_token, :password_digest, presence: true
+    validates :password, length: { minimum: 6, allow_nil: true }
+    
+    before_validation :ensure_session_token
+    attr_reader :password
+
+    
+
+    def reset_session_token!
+        self.session_token = SecureRandom::urlsafe_base64(16)
+        self.save!
+        self.session_token
+    end
+    
+    def ensure_session_token
+        self.session_token ||= SecureRandom::urlsafe_base64(16)
+    end
+
+    def password=(password)
+        @password = password
+        self.password_digest = BCrypt::Password.create(password)
+    end
+
+    def is_password?(password)
+        BCrypt::Password.new(self.password_digest).is_password?(password)
+    end
+
+    def self.find_by_credentials(username, password)
+        user = User.find_by(username: username)
+        user && user.is_password?(password) ? user : nil
+    end
+end
