6f8fa0f135620c2243c46d4e04bb19a8578afd36
jayfang88 <jayfang88@gmail.com>
2020-07-09 18:40:05 -0700

    add user auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
     validates :username, uniqueness: true
-    validates :username, :fname, :lname, :session_token, :password_digest, presence: true
+    validates :username, :name, :session_token, :password_digest, presence: true
     validates :password, length: { minimum: 6, allow_nil: true }
     
     before_validation :ensure_session_token
--- frontend/movie_rate.jsx
+++ frontend/movie_rate.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { login } from './util/session_api_util';
 
 document.addEventListener("DOMContentLoaded", () => {
     const root = document.getElementById("root");
+
+    window.login = login
     ReactDOM.render(<h1>Welcome to MovieRate</h1>, root);
 });
