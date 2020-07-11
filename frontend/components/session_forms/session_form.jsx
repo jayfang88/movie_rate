99cc5979f58fdaf5f6a8c9f105648bd1584e244e
jayfang88 <jayfang88@gmail.com>
2020-07-10 17:59:41 -0700

    add session form styling

--- frontend/components/session_forms/session_form.jsx
+++ frontend/components/session_forms/session_form.jsx
@@ -41,43 +41,45 @@ class SessionForm extends React.Component {
     render() {
         let signingUp = (this.props.formType === 'Sign Up');
         return (
-            <div className='session-form'>
-                <h3>{this.props.formType}</h3>
-                <br/>
-                {this.renderErrors()}
-                <br/>
-                <form className='session-form-box'>
-                    <label className='session-form-section'>
-                        Username<br/>
-                        <input className='session-form-input' type="text"
-                            value={this.state.username} onChange={this.update('username')}/>
-                    </label>
+            <div className='session-form-page'>
+                <div className='session-form'>
+                    <h3>{this.props.formType}</h3>
                     <br/>
-                    {signingUp ? (
+                    {this.renderErrors()}
+                    <br/>
+                    <form className='session-form-box'>
                         <label className='session-form-section'>
-                            Name<br />
+                            Username<br/>
                             <input className='session-form-input' type="text"
-                                value={this.state.name} onChange={this.update('name')} />
+                                value={this.state.username} onChange={this.update('username')}/>
                         </label>
+                        <br/>
+                        {signingUp ? (
+                            <label className='session-form-section'>
+                                Name<br />
+                                <input className='session-form-input' type="text"
+                                    value={this.state.name} onChange={this.update('name')} />
+                            </label>
+                        ) : (
+                            <div></div>
+                        )}
+                        {signingUp ? ( <br/> ) : ( <div></div> )}
+                        <label className='session-form-section'>
+                            Password<br/>
+                            <input className='session-form-input' type="password"
+                                value={this.state.password} onChange={this.update('password')}/>
+                        </label>
+                        <br/>
+                        <button className='session-form-btn' onClick={this.handleSubmit}>
+                            {this.props.formType}
+                        </button>
+                    </form>
+                    {signingUp ? (
+                        <p>Have an account?<Link className='switch-session-form' to='/login'>Log in instead</Link></p>
                     ) : (
-                        <div></div>
+                        <p>Don't have an account?<Link className='switch-session-form' to='/signup'>Sign up here</Link></p>
                     )}
-                    {signingUp ? ( <br/> ) : ( <div></div> )}
-                    <label className='session-form-section'>
-                        Password<br/>
-                        <input className='session-form-input' type="password"
-                            value={this.state.password} onChange={this.update('password')}/>
-                    </label>
-                    <br/>
-                    <button className='session-form-btn' onClick={this.handleSubmit}>
-                        {this.props.formType}
-                    </button>
-                </form>
-                {signingUp ? (
-                    <p>Have an account?<Link className='switch-session-form' to='/login'>Log in instead</Link></p>
-                ) : (
-                    <p>Don't have an account?<Link className='switch-session-form' to='/signup'>Sign up here</Link></p>
-                )}
+                </div>
             </div>
         )
     }
