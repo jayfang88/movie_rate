146bac89d750e21906601614360e21fddb57e66d
jayfang88 <jayfang88@gmail.com>
2020-07-14 19:44:20 -0700

    setup ActiveStorage w/ AWS

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,5 +2,7 @@ class Movie < ApplicationRecord
     validates :title, uniqueness: true
     validates :title, :year_released, :score, :description, presence: true
 
+    has_one_attached :photo
+
     #has_many :comments
 end
