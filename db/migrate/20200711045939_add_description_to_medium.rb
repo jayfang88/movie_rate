79e94ce0f8e392986f39198bca46ed09c49e3d0e
jayfang88 <jayfang88@gmail.com>
2020-07-22 23:16:13 -0700

    remove migrations

--- db/migrate/20200711045939_add_description_to_medium.rb
+++
@@ -1,5 +0,0 @@
-class AddDescriptionToMedium < ActiveRecord::Migration[5.2]
-  def change
-    add_column :media, :description, :text, null: false
-  end
-end
--- db/migrate/20200711051311_fix_media_column_name.rb
+++
@@ -1,5 +0,0 @@
-class FixMediaColumnName < ActiveRecord::Migration[5.2]
-  def change
-    rename_column :media, :type, :type_of
-  end
-end
