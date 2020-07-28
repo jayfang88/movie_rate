ba12c56b3d73cbe93b1fdc237fe65c852c74a4b2
jayfang88 <jayfang88@gmail.com>
2020-07-28 13:55:00 -0700

    comment out comment controllers

--- app/controllers/api/comments_controller.rb
+++ app/controllers/api/comments_controller.rb
@@ -3,9 +3,9 @@ class Api::CommentsController < ApplicationController
         @comments = Comment.all
     end
 
-    def show
-        @comment = Comment.find_by(id: params[:id])
-    end
+    # def show
+    #     @comment = Comment.find_by(id: params[:id])
+    # end
 
     def create
         @comment = Comment.new(comment_params)
@@ -16,14 +16,14 @@ class Api::CommentsController < ApplicationController
         end
     end
 
-    def update
-        @comment = Comment.find_by(id: params[:comment][:id])
-        if @commment.update(comment_params)
-            render :show
-        else
-            render json: @comment.errors.full_messages, status: 422
-        end
-    end
+    # def update
+    #     @comment = Comment.find_by(id: params[:comment][:id])
+    #     if @commment.update(comment_params)
+    #         render :show
+    #     else
+    #         render json: @comment.errors.full_messages, status: 422
+    #     end
+    # end
     
     def destroy
         @comment = Comment.find_by(id: params[:id])
